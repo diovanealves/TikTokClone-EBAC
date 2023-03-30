21b578f3edbcbb6735249afe5a52823d74032669
Diovane Alves <alvdiovane@gmail.com>
2023-03-29 23:11:40 -0300

    🚧 Criando sistema de Play Pause

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,8 +1,31 @@
+import { useRef, useState } from "react";
+import videoTest from "../../assets/videoTest.mp4";
+
 export default function Video() {
+  const [play, setPlay] = useState(false);
+  const videoRef = useRef(null);
+  const volume = 0.5;
+
+  function handleStartVideo() {
+    if (play) {
+      videoRef.current.pause();
+      setPlay(false);
+    } else {
+      videoRef.current.play();
+      videoRef.current.volume(volume);
+      setPlay(true);
+    }
+  }
+
   return (
-    <div className="relative bg-black text-white h-full w-full snap-start">
+    <div className="relative h-full w-full snap-start">
       index
-      <video className="h-full w-full object-fill" src=""></video>
+      <video
+        className="h-full w-full object-fill"
+        ref={videoRef}
+        onClick={handleStartVideo}
+        src={videoTest}
+      ></video>
     </div>
   );
 }
