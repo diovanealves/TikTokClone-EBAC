f8a240d917fe6b39de338a73e27fd6feb2fb5b6d
Diovane Alves <alvdiovane@gmail.com>
2023-03-29 23:07:10 -0300

    🚧 Criando componente Video

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import "./style/global.css";
+import Video from "./components/Video";
 
 export default function App() {
-  return <div> </div>;
+  return (
+    <div className="h-screen grid place-items-center">
+      <div className="h-screen max-h-[800px] w-4/5 max-w-[500px] rounded-2xl">
+        <Video />
+      </div>
+    </div>
+  );
 }
