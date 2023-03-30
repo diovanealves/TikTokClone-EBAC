b3a09f75f36b818e4f632a401456ccbd063d6972
Diovane Alves <alvdiovane@gmail.com>
2023-03-29 23:08:45 -0300

    ✨ feat scroll mandatory

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Video from "./components/Video";
 export default function App() {
   return (
     <div className="h-screen grid place-items-center">
-      <div className="h-screen max-h-[800px] w-4/5 max-w-[500px] rounded-2xl">
+      <div className="relative h-screen max-h-[800px] w-4/5 max-w-[500px] overflow-scroll rounded-2xl snap-y snap-mandatory app_Video">
         <Video />
       </div>
     </div>
--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,3 +1,8 @@
 export default function Video() {
-  return <div>index</div>;
+  return (
+    <div className="relative bg-black text-white h-full w-full snap-start">
+      index
+      <video className="h-full w-full object-fill" src=""></video>
+    </div>
+  );
 }
