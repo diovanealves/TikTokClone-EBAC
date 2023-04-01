ea40bbabac3fde2ed107215e1812659955d3d1dc
Diovane Alves <alvdiovane@gmail.com>
2023-04-01 19:51:53 -0300

    :iphone: Fix Responsividade para Mobile

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Video from "./pages/Video";
 export default function App() {
   return (
     <div className="h-screen grid place-items-center">
-      <div className="relative h-5/6 md:h-screen max-h-[800px] w-4/5 max-w-[500px] overflow-scroll rounded-2xl snap-y snap-mandatory app_Video">
+      <div className="relative h-[90%] w-11/12 max-w-[500px] overflow-scroll rounded-2xl snap-y snap-mandatory app_Video">
         <Video
           likes={100}
           messages={30}
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,19 +3,19 @@ import Vinil from "../../assets/Vinil.png";
 
 export default function Footer({ name, description, music }) {
   return (
-    <div className="relative bottom-[20%] ml-5">
+    <div className="relative bottom-[23%] md:bottom-[15%] ml-5">
       <div>
         <h1 className="text-xl font-bold pb-3">{name}</h1>
         <p className="pb-5">{description}</p>
         <div className="flex gap-2 pb-2">
           <MusicNoteIcon className="" />
           <div className="w-4/5 overflow-hidden">
-            <p className="translate-x-96  animate-slide">{music}</p>
+            <p className="text-sm translate-x-96  animate-slide">{music}</p>
           </div>
         </div>
       </div>
       <img
-        className="absolute bottom-0 right-3 h-10 invert animate-spin"
+        className="absolute bottom-0 right-4 h-10 invert animate-spin"
         alt="Imagem de um vinil girando"
         src={Vinil}
       />
