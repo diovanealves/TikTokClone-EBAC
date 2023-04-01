127829ad15ebff5dbf1504a72a062d2022b10d44
Diovane Alves <alvdiovane@gmail.com>
2023-04-01 12:13:16 -0300

    :construction: Finalizando SideBarVideo

--- src/components/SidebarBar/index.js
+++ src/components/SidebarBar/index.js
@@ -1,3 +1,33 @@
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ChatIcon from "@mui/icons-material/Chat";
+import ShareIcon from "@mui/icons-material/Share";
+import { useState } from "react";
+
 export default function VideoSideBar() {
-  return <div className="absolute top-[50%] right-2 text-white"></div>;
+  const [like, setLike] = useState(false);
+  function handdleLike() {
+    setLike(!like);
+  }
+
+  return (
+    <div className="absolute top-[50%] right-0 text-white">
+      <div className="p-5 text-center" onClick={handdleLike}>
+        {like ? (
+          <FavoriteIcon fontSize="large" />
+        ) : (
+          <FavoriteBorderIcon fontSize="large" />
+        )}
+        <p>{like ? 300 + 1 : 300}</p>
+      </div>
+      <div className="p-5 text-center">
+        <ChatIcon fontSize="large" />
+        <p>10</p>
+      </div>
+      <div className="p-5 text-center">
+        <ShareIcon fontSize="large" />
+        <p>32</p>
+      </div>
+    </div>
+  );
 }
